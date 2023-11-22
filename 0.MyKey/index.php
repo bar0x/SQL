e537dc68d48279225b70399a842a3fda1c9cfb64
Mattia Baroni <92336539+bar0x@users.noreply.github.com>
2023-11-22 20:12:59 +0100

    fix

--- 0.MyKey/index.php
+++ 0.MyKey/index.php
@@ -144,26 +144,5 @@ printTotalFromName("Davide",$location, $user, $password, $dbName);
 
 echo("</tbody> </table>");
 require("foot.php");
-
-function printTotalFromName($fname,$_location, $_user, $_password, $_dbName) {
-    //faccio la query
-    $db = new mysqli($_location,$_user,$_password,$_dbName); 
-    $sql = "
-    SELECT nome, sum(saldo), data_transazione
-    FROM ricarica
-    WHERE nome = '$fname'
-    ";
-    $resultSet = $db->query($sql);
-    $db->close();
-
-    //stampo i risultati
-    $record = $resultSet->fetch_assoc();
-    echo("<tr>
-        <td>".$record['nome']."</td>
-        <td>".$record['sum(saldo)']." €</td>
-        <td>".$record['data_transazione']."</td>
-    ");
-    }
-
-
+require("functions.php");
 ?>
