cc27c431efcb2c3c0f202d4324ca58663ce3d4a9
Mattia Baroni <92336539+bar0x@users.noreply.github.com>
2024-01-16 10:18:18 +0100

    fix ver

--- CS1_7pag123/2.0/php/index.php
+++ CS1_7pag123/2.0/php/index.php
@@ -7,7 +7,7 @@
 require("../include/head.php");
 require("../include/funz.php");
     scriviNavbar();
-    echo('<div class="alert alert-primary">Da qui si inizia a costruire la UI - Ver 1.0</div>');
+    echo('<div class="alert alert-primary">Da qui si inizia a costruire la UI - Ver 2.0</div>');
 
 
     require("../include/foot.php");
